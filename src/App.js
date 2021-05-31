1fd5c241fa3de02e39270cf8ee2dc798f7c91007
Lan Nagashima <Lan@Lans-MacBook-Pro.local>
2021-05-31 10:58:03 -0700

    anecdote project

--- src/App.js
+++ src/App.js
@@ -1,90 +1,65 @@
 import React, { useState } from 'react'
-const GoodClick = (props) =>{
-  return (
-   <button onClick = {props.handleclick}>good</button>
+const NextAnec = (props) =>{
+  return(
+    <button onClick = {props.handleclick}>Next Anecdote!</button>
   )
 }
-const Statistics = ({clicks,setClick})=>{
-
-  const NeutralClick = () =>{
-    const newClicks = {
-      ...clicks, 
-      neutral: clicks.neutral+1,
-      allClicks: clicks.allClicks+1,
-      average: (clicks.good-clicks.bad)/(clicks.allClicks+1),
-      positive: clicks.good/(clicks.allClicks+1)*100
-    }
-    setClick(newClicks)
-  
-  }
-  const BadClick = () =>{
-    const newClicks = {
-      ...clicks, 
-      bad: clicks.bad+1,
-      allClicks: clicks.allClicks+1,
-      average: (clicks.good-clicks.bad-1)/(clicks.allClicks+1),
-      positive: clicks.good/(clicks.allClicks+1)*100
-    }
-    setClick(newClicks)
-  
-  }
-    const FeedBack = ()=>{
-      if(clicks.bad===0&&clicks.good===0&&clicks.neutral===0){
-        return(
-          <div>
-            No Feed Back given
-          </div>
-        )
-      }
-      else{
-        return(
-          <div>
-          <table><thead><tr><th>Statistics</th></tr></thead><tbody><tr><th>Condition</th><th>Number</th></tr><tr><td>Bad:</td><td>{clicks.bad}</td></tr><tr><td>Neutral:</td><td>{clicks.neutral}</td></tr><tr><td>Good:</td><td>{clicks.good}</td></tr><tr><td>All:</td><td>{clicks.allClicks}</td></tr><tr><td>Average:</td><td>{clicks.average}</td></tr><tr><td>Positive:</td><td>{clicks.positive}</td></tr></tbody></table>
-          </div>
-
-        )
-
-      }
-    }
-  
-
-  return (
-
-    <div>
-      <h1>Give FeedBacks</h1>    
-      <button onClick = {BadClick}>bad</button>
-      <button onClick = {NeutralClick}>neutral</button>
-      <br></br>
-      <FeedBack/>
-    </div>
-
+const Vote = (props) =>{
+  return(
+    <button onClick = {props.handleclick}>Vote!</button>
   )
-
 }
 const App = () => {
-  const [clicks, setClick ] = useState({
-    good:0,neutral:0,bad:0,allClicks:0,average:0,positive:0
-  })
-  const goodi = ()=> {
-    const newClicks = {
-      ...clicks, 
-      good: clicks.good+1,
-      allClicks: clicks.allClicks+1,
-      average: (clicks.good+1-clicks.bad)/(clicks.allClicks+1),
-      positive: (clicks.good+1)/(clicks.allClicks+1)*100
-    
-    }
-    setClick(newClicks)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+   
+  const [selected, setSelected] = useState(Math.floor(Math.random()*5))
+  const [vote, setVote] = useState({'arr':[0,0,0,0,0,0],'most':0})
+  const next = () => {
+      const nAnec = Math.floor(Math.random()*5)
+      setSelected(nAnec)
   }
-  return(
+  const voting = () =>{
+    let newArray = []
+    let c = vote['most']
+    const newVote = vote['arr'][selected]+1
+    for(let i = 0; i<vote['arr'].length;i++){
+      if(selected===i){
+        newArray.push(newVote)
+        continue;
+      }
+      newArray.push(vote['arr'][i])
+    }
+    if(vote['arr'][vote['most']]<newVote){
+        c = selected
+     }
+    const newDict = {
+      'arr':newArray,
+      'most': c
+    }
+    setVote(newDict)
+  }
+  return (
     <div>
-      <Statistics clicks = {clicks} setClick = {setClick}/>
-      <GoodClick handleclick = {goodi}/>
+      {anecdotes[selected]}
+      <br></br>
+      Vote: {vote['arr'][selected]}
+      <br></br>
+      <NextAnec handleclick = {next}/>
+      <Vote handleclick = {voting}/>
+
+      <h1>Most Vote</h1>
+      {anecdotes[vote['most']]}
+      <br></br>
+      Vote Number:{vote['arr'][vote['most']]}
     </div>
-
   )
-
 }
-
 
 export default App
